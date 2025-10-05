c342e302faaae62c719635634c05988de0603ff3
dhananjay421366 <nimbalkardhananjay349@gmail.com>
2025-10-06 00:42:33 +0530

    Learning backtracking

---
+++ Backtracking/N_Queens.java
@@ -0,0 +1,76 @@
+
+public class N_Queens {
+
+    private static boolean isSafe(char[][] board, int row, int col) {
+        // check vertical up 
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // check diag up left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // check diag up right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public static int N_Queens(char board[][], int row) {
+        int count = 0;
+        if (board.length == row) {
+            printBoard(board);
+            return 1;
+        }
+
+        // col 
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q'; // set the queen
+                count += N_Queens(board, row + 1);
+                board[row][j] = 'X';
+            }
+        }
+        return count;
+    }
+
+    public static void printBoard(char board[][]) {
+
+        System.out.println("-------Chess Board----------");
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        char board[][] = new char[n][n];
+        // initialize 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+
+        System.out.println(N_Queens(board, 0));
+
+    }
+
+}
--- Backtracking/Subsets.java
+++ Backtracking/Subsets.java
@@ -1,11 +1,29 @@
 
 public class Subsets {
 
-    public static void Subsets(String str) {
-        
+    public static void Subsets(String str, StringBuilder ans, int i) {
+        // base case 
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.print("Null" + " ");
+            } else {
+                System.out.print(ans + "  ");
+            }
+            return;
+        }
+
+        // yes choice 
+        Subsets(str, new StringBuilder(ans).append(str.charAt(i)), i + 1);
+
+        // no choice
+        Subsets(str, ans, i + 1);
+
     }
 
     public static void main(String[] args) {
+        String str = "abc";
+        // Subsets(str, "", 0);
+        Subsets(str, new StringBuilder(), 0);
 
     }
 }
